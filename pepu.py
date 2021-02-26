22b06d162849edff090790079c14a497eaf8b293
Joel Himanen <joel.himanen@aalto.fi>
2021-02-26 10:29:07 +0200

    Add infoprints to the main loop

--- pepu.py
+++ pepu.py
@@ -11,30 +11,57 @@ def main():
 
     random.shuffle(attendees)
 
+    print("\n" + "Aloitetaan arvonta.")
+    time.sleep(3)
     print("\n" + "Ensimmäisenä putoaa..." + "\n")
     time.sleep(3)
     print(attendees.pop() + "\n")
 
     while len(attendees) > 1:
-        loser = attendees.pop()
-        time.sleep(2)
-        print("Seuraavana putoaa..." + "\n")
-        time.sleep(3)
-        print(loser + "\n")
 
-    winner = attendees.pop()
+        if len(attendees) == 5:
+            time.sleep(2)
+            print("\n"+ "Enää viisi kilpailijaa jäljellä!" + "\n")
+            time.sleep(2)
+            print("Top 5 on: " + "\n")
 
-    time.sleep(2)
-    print("Ja voittaja on..." + "\n")
+            for person in attendees:
+                print(person)
 
-    time.sleep(3)
-    print("Jännitys tiivistyy..." "\n")
+            random.shuffle(attendees)
+            time.sleep(6)
+            print("\n" + "Jatketaan arvontaa!" + "\n")
+            time.sleep(2)
+            print("Seuraava putoaja on..." + "\n")
+            time.sleep(2)
+            print(attendees.pop() + "\n")
 
-    time.sleep(3)
-    print(winner.upper() + "!!!" + "\n")
-    
-    time.sleep(1)
-    print("Onnea voittajalle!" + "\n")
+        elif len(attendees) == 2:
+            time.sleep(2)
+            print("\n" + "Jäljellä on enää kaksi finalistia!" + "\n")
+            time.sleep(1)
+            print("He ovat " + attendees[0] + " ja " + attendees[1] + "!")
+
+            random.shuffle(attendees)
+            time.sleep(4)
+            print("\n" + "Jännitys tiivistyy..." + "\n")
+            time.sleep(2)
+            print("Rumpujen pärinää..." + "\n")
+            time.sleep(3)
+            print("Tämän perjantaipullon voittaa....." + "\n")
+            
+            winner = attendees.pop()
+            time.sleep(3)
+            print(winner.upper() + "!!!" + "\n")
+            
+            time.sleep(1)
+            print("Onnea voittajalle!" + "\n")
+
+        else:
+            time.sleep(2)
+            print("Seuraavana putoaa..." + "\n")
+            time.sleep(3)
+            print(attendees.pop() + "\n")
 
 
 if __name__ == "__main__":
